8434d931fd85d59d822597bdca29d1a11a7fdb3e
om2013ab <omarbashaiwth@gmail.com>
2022-09-15 00:30:44 +0800

    Initial commit

---
+++ app/src/main/java/com/omarbashawith/mufeed_app/core/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.omarbashawith.mufeed_app.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+// The names of color from: https://www.color-name.com/hex/
+// Tags color
+val CoralRed = Color(0xFFF44336)
+val Midnight = Color(0xFF611A6C)
+val PomonaGreen = Color(0xFF114A2F)
+val BluePigment = Color(0xFF232F97)
+val BlueSapphire = Color(0xFF145F69)
+
+//Light Colors
+val BrightGray = Color(0xFFEDEDED)
+val OldGold = Color(0xFFCBB43E)
+val White = Color(0xFFFFFFFF)
+val GraniteGray = Color(0xFF5F5F5F)
+val PhilippineGray = Color(0xFF8F8F90)
+
+
+//Dark Colors
+val ChineseBlack = Color(0xFF101010)
+val EerieBlack = Color(0xFF1F1F1F)
+val Gainsboro = Color(0xFFDADADA)
+val BistreBrown = Color(0xFF8B6B19)
+
